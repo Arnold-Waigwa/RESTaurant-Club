1d8370d95e575f66a8747d03c1d44e8e496f8be8
Arnold Waigwa <waigwaarnold@gmail.com>
2024-10-25 11:42:38 -0400

    More css change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [data, setData] = useState([]);
@@ -72,6 +73,7 @@ function App() {
 
   return (
     <center>
+      <Navbar></Navbar>
       <div className="App">
         <h1>Weather Data Dashboard</h1>
 
